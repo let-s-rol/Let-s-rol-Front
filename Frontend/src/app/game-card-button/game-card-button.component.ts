1842a8a7331d385a3c73226751897527235cd7b3
IagoPL <iagoprietolamas@gmail.com>
2023-03-09 16:38:37 +0100

    ajustar botones

--- Frontend/src/app/game-card-button/game-card-button.component.ts
+++ Frontend/src/app/game-card-button/game-card-button.component.ts
@@ -20,15 +20,6 @@ export class GameCardButtonComponent implements OnInit {
 
         "img":"https://imgs.search.brave.com/Wt2sdEpSRr9rzDciZmT6BA3C5PkUg2sQSuAdemfr350/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/cGluY2xpcGFydC5j/b20vcGljZGlyL2Jp/Zy81NjctNTY3NzAy/MV9raW5nZG9tLWhl/YXJ0cy0xLWFydHdv/cmstY2xpcGFydC5w/bmc"
 
-      },{ 
-
-        "name":"Dungeon & Dragons",
-
-        "bg":"https://i.pinimg.com/736x/2b/b0/62/2bb06297906fd0759245abbca5168173.jpg",
-
-        "img":"https://cdn.pixabay.com/photo/2021/01/28/14/47/druid-5958196_1280.png",
-        "height":"20px"
-
       } ,{ 
 
         "name":"Dungeon & Dragons",
