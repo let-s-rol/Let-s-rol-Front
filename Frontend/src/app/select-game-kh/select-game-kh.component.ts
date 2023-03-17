023a7768a848df33ff306ca8107b7c59e8aba825
IagoPL <iagoprietolamas@gmail.com>
2023-03-17 18:44:29 +0100

    json de runs

--- Frontend/src/app/select-game-kh/select-game-kh.component.ts
+++ Frontend/src/app/select-game-kh/select-game-kh.component.ts
@@ -4,31 +4,46 @@ import { Run } from '../inferfaces/run';
 @Component({
   selector: 'app-select-game-kh',
   templateUrl: './select-game-kh.component.html',
-  styleUrls: ['./select-game-kh.component.css']
+  styleUrls: ['./select-game-kh.component.css'],
 })
 export class SelectGameKHComponent implements OnInit {
-
   cards: Run[];
 
-    constructor() {
-      const UserJSON: string = `{
-        "runs": [{ 
-  
-           000000
-  
+  constructor() {
+    const UserJSON: string = `{
+        "runs": [
+          { 
+
+            "name" : "panas",
+            "id" : "001", 
+            "idGame" : "001",
+           "description": "una gean Partida con illojuan"
+
+        } ,
+          
+          { 
+
+            "name" : "xavi te falta calle",
+            "id" : "002", 
+            "idGame" : "001",
+           "description": "una gean Partida con illojuan"
+
+        } ,
+          { 
+
+            "name" : "albino",
+            "id" : "003", 
+            "idGame" : "001",
+           "description": "una gean Partida con illojuan"
+
         } 
+
       ]
         }`;
-  
-      const userDict: any = JSON.parse(UserJSON);
-      this.cards = userDict['runs'];
-    }
 
-    
-    
-   
-
-  ngOnInit(): void {
+    const userDict: any = JSON.parse(UserJSON);
+    this.cards = userDict['runs'];
   }
 
+  ngOnInit(): void {}
 }
