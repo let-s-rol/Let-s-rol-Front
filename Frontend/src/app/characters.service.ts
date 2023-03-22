48a6714a09aa6cb93f23aeb2966649f777033864
LeojMada <joelarnedo@gmail.com>
2023-03-22 20:08:47 +0100

    Character Base Form Completado

---
+++ Frontend/src/app/characters.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BaseCharacter } from './inferfaces/baseCharacter';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CharactersService {
+  CharactersServiceComponentFactory(CharactersService: CharactersService) {
+    throw new Error('Method not implemented.');
+  }
+
+  readonly Url = 'http://127.0.0.1:8000/api/';
+
+  constructor(private http: HttpClient) { }
+
+  baseCharacterData: any;
+
+  addBaseCharacter(baseCharacter: BaseCharacter) {
+    return this._http
+      .post(this.Url + 'register', baseCharacter)
+      .subscribe((response) => {
+        let found = false;
+        if (response != null) {
+          found = true;
+        }
+        this.baseCharacterData = response;
+        return found;
+      });
+  }
+
+
+
+}
