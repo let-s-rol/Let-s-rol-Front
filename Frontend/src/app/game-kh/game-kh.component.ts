c9545b4bc901a2ddddb15e44471ccc6024416687
LeojMada <joelarnedo@gmail.com>
2023-05-16 19:44:15 +0200

    Entra en Run con ID ranking en URL y pilla Characters para Chequear si tienen

--- Frontend/src/app/game-kh/game-kh.component.ts
+++ Frontend/src/app/game-kh/game-kh.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { CompleteRunManagamentService } from '../service/complete-run-managament.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FullCharacter } from '../inferfaces/fullCharacter';
 
 @Component({
   selector: 'app-game-kh',
@@ -9,9 +12,38 @@ export class GameKhComponent implements OnInit {
 
 // TODO hacer enlace
   createCharacter : boolean = false
-  constructor() { }
+  constructor(
+    private CompleteRunManagamentService: CompleteRunManagamentService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private cdr: ChangeDetectorRef
+  ) { }
+
+  FullCharacter!: FullCharacter [];
 
   ngOnInit(): void {
+
+             // Obtiene el ranking de estudiantes del servidor a través del servicio InputService/StudentRankingManagament
+             this.route.params.subscribe((params) => {
+              let id = Number.parseInt(params['id']);
+              if (Number.isNaN(id)) {
+                console.error('Invalid id:', params['id']);
+                return;
+              }
+              console.log('ID from route params:', id);
+              this.CompleteRunManagamentService.getCharacterTable(id).subscribe((response: FullCharacter[]) => {
+                this.FullCharacter = response;
+              });
+            });
+    
+
   }
 
+  proofCharacterExist() {
+
+  
+ 
+
+
+}
 }
