f667de3c1977718f9a26e8f6806c6421f8b6b0a4
IagoPL <iagoprietolamas@gmail.com>
2023-05-17 18:51:56 +0200

    select option

--- Frontend/src/app/game-kh/new-character-pannel/new-character-pannel.component.ts
+++ Frontend/src/app/game-kh/new-character-pannel/new-character-pannel.component.ts
@@ -1,7 +1,7 @@
 
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { CompleteRunManagamentService } from '../../service/complete-run-managament.service';
+import { CompleteRunManagamentService } from 'src/app/service/complete-run-managament.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FullCharacter } from '../../inferfaces/fullCharacter';
 
@@ -22,7 +22,11 @@ export class NewCharacterPannelComponent implements OnInit {
 
 
 
-  constructor(public router: Router) {
+  constructor(    private CompleteRunManagamentService: CompleteRunManagamentService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private cdr: ChangeDetectorRef
+    ) {
     this.character = new FormGroup({
       name: new FormControl('', [Validators.required]),
       race: new FormControl('', [Validators.required]),
@@ -44,8 +48,7 @@ export class NewCharacterPannelComponent implements OnInit {
   ngOnInit(): void {
 
        //GET DE BASE CHARACTERS
-       //GET DE BASE CHARACTERS
-    this['CompleteRunManagamentService'].getBaseCharacters().subscribe(
+       this.CompleteRunManagamentService.getBaseCharacters().subscribe(
         (response: FullCharacter[]) => {
           this.baseCharacter = response;
           console.log('Personajes Base: ', this.baseCharacter);
