02604e3372caed82833f9c4bc22b9f7e3751c98d
IagoPL <iagoprietolamas@gmail.com>
2023-05-17 16:54:47 +0200

    qwe

--- Frontend/src/app/game-kh/game-kh.component.ts
+++ Frontend/src/app/game-kh/game-kh.component.ts
@@ -6,44 +6,48 @@ import { FullCharacter } from '../inferfaces/fullCharacter';
 @Component({
   selector: 'app-game-kh',
   templateUrl: './game-kh.component.html',
-  styleUrls: ['./game-kh.component.css']
+  styleUrls: ['./game-kh.component.css'],
 })
 export class GameKhComponent implements OnInit {
+  // TODO hacer enlace
+  createCharacter: boolean = true;
+  character!: FullCharacter[];
+  
+  characterName!:string;
 
-// TODO hacer enlace
-  createCharacter : boolean = false
   constructor(
     private CompleteRunManagamentService: CompleteRunManagamentService,
     private router: Router,
     private route: ActivatedRoute,
     private cdr: ChangeDetectorRef
-  ) { }
-
-  FullCharacter!: FullCharacter [];
+  ) {}
 
   ngOnInit(): void {
-
-             // Obtiene el ranking de estudiantes del servidor a través del servicio InputService/StudentRankingManagament
-             this.route.params.subscribe((params) => {
-              let id = Number.parseInt(params['id']);
-              if (Number.isNaN(id)) {
-                console.error('Invalid id:', params['id']);
-                return;
-              }
-              console.log('ID from route params:', id);
-              this.CompleteRunManagamentService.getCharacterTable(id).subscribe((response: FullCharacter[]) => {
-                this.FullCharacter = response;
-              });
-            });
-    
-
+    // Obtiene el ranking de estudiantes del servidor a través del servicio InputService/StudentRankingManagament
+    this.route.params.subscribe((params) => {
+      let id = Number.parseInt(params['id']);
+      if (Number.isNaN(id)) {
+        console.error('Invalid id:', params['id']);
+        return;
+      }
+      console.log('ID from route params:', id);
+      this.CompleteRunManagamentService.getCharacterTable(id).subscribe(
+        (response) => {
+          this.character = response;
+          this.characterName= this.character[0].name;
+          console.log('Character: ', this.character);
+          console.log('Name', this.characterName);
+          
+          if (this.character[0] && this.character[0].name !== null && this.character[0].name !== undefined) {
+            console.log('El Nombre:', this.character[0].name);
+            this.createCharacter = false;
+          } else {
+            console.log('Name property does not exist or has null/undefined value');
+          }
+        }
+      );
+    });
   }
 
-  proofCharacterExist() {
-
-  
- 
-
-
-}
+  proofCharacterExist() {}
 }
