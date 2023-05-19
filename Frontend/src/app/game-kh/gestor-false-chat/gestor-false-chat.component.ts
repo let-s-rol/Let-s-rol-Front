d19722c368f708b8ee05c5d07f77b178c2b0fef8
LeojMada <joelarnedo@gmail.com>
2023-05-19 13:09:29 +0200

    Pilla nombre del Personaje en el Chat

--- Frontend/src/app/game-kh/gestor-false-chat/gestor-false-chat.component.ts
+++ Frontend/src/app/game-kh/gestor-false-chat/gestor-false-chat.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { CompleteRunManagamentService } from 'src/app/service/complete-run-managament.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FullCharacter } from '../../inferfaces/fullCharacter';
 
 @Component({
   selector: 'app-gestor-false-chat',
@@ -8,12 +12,50 @@ import { Component, OnInit } from '@angular/core';
 export class GestorFalseChatComponent implements OnInit {
   userMessages: string[] = [];
   userInput: string = ''; // Initialize the userInput property
+  character!: FullCharacter[];
+
+    constructor(
+    private CompleteRunManagamentService: CompleteRunManagamentService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private cdr: ChangeDetectorRef
+  ) { }
 
   saveUserMessage() {
-    this.userMessages.push(this.userInput);
+    if (this.character && this.character.length > 0) {
+      const characterName = this.character[0].name;
+  
+  +
+      this.userMessages.push(this.userInput);
+    }
+  
     this.userInput = ''; // Clear the userInput after saving the message
   }
-
+  
+  
+  
+  
   ngOnInit(): void {
+
+        //GUARDA EN UNA VARIABLE LA ID DE LA RUN QUE ESTÁ EN LA URL
+        this.route.params.subscribe((params) => {
+          let id = Number.parseInt(params['id']);
+          if (Number.isNaN(id)) {
+            console.error('Invalid id:', params['id']);
+            return;
+          }
+    
+
+    console.log('ID from route params:', id);
+    this.CompleteRunManagamentService.getCharacterTable(id).subscribe(
+      (response: FullCharacter[]) => {
+        this.character = response;
+        console.log('Character: ', this.character);
+ 
+      }
+    );
   }
+)};
+
 }
+
